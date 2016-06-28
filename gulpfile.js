8d46f3e9a06f520555f66221fbb6d1e6f8c9d9b3
oleg <o.schekotihin@gmail.com>
2016-06-28 15:46:19 +0300

    add AboutEU and fixing bugs

--- gulpfile.js
+++ gulpfile.js
@@ -34,7 +34,9 @@ gulp.task('browserSync', function() {
 
 gulp.task('sass', function() {
   return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
-    .pipe(sass()) // Passes it through a gulp-sass
+    .pipe(sass({
+      includePaths: ['./bower_components/breakpoint-sass/stylesheets']
+    }))
     .pipe(gulp.dest('app/css')) // Outputs it in the css folder
     .pipe(browserSync.reload({ // Reloading with Browser Sync
       stream: true
